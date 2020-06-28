72c8525f8d3f33d1f0004fdc3d505731a87341c6
Janosh Riebesell <janosh.riebesell@gmail.com>
2020-06-28 15:49:28 +0200

    feat: inline all SVGs smaller than 15 kB using dataURIs, fix SVGs that broke inline due to invalid font-family

--- src/components/PostExcerpt/styles.js
+++ src/components/PostExcerpt/styles.js
@@ -19,7 +19,7 @@ export const Post = styled.article`
 `
 
 export const Cover = styled(Img).attrs(
-  ({ fluid, src }) => !fluid && { as: src ? `img` : `div` }
+  p => !p.fluid && p.src && { as: `img`, src: p.dataURI || p.src }
 )`
   height: calc(10em + 4vh);
   width: 100%;
